b8ee05b2acc3e0f4e0a8f11355b9656e6419ac29
ammrshmbng <ammarmare01@gmail.com>
2024-11-07 16:41:05 +0700

    log the inView variable

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      console.log("inView", inView);
+    }
+  }, [inView]);
+
+  console.log(inView);
+
   return (
     <>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
